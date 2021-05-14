07c7cb11724ae86914bd171dd33c3ce0b50c788a
Freya Claes <freya.claes@student.ehb.be>
2021-05-14 11:28:45 +0200

    uml

--- js/index.js
+++ js/index.js
@@ -2,7 +2,18 @@
 
 
 function init() {
-
+    
+/*     const filtered = cells.filter((field) => 
+    (
+    Math.abs(field.x - this.x) <= 1 &&
+    Math.abs(field.y - this.y) <= 1
+    ) &&
+    !(
+    field.x == this.x && 
+    field.y == this.y 
+    ) &&
+    field.state == 1 
+    ); */
 }
 
 
